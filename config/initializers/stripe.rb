61b84e6ed2013954265298a1c5352471cf447121
neogalaxy45@gmail.com <neogalaxy45@gmail.com>
2018-04-13 16:52:06 +0700

    deploy to heroku again

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_kiHNC5vzFBpS5sZjpLSIE3Sz',
-  :secret_key      => 'sk_test_8InElYumqplujiTczOJDkTdY'
+  :publishable_key => ENV['stripe_publishable_key'],
+  :secret_key      => ENV['stripe_api_key']
 }
 
-Stripe.api_key = 'sk_test_8InElYumqplujiTczOJDkTdY'
+Stripe.api_key = ENV['stripe_api_key']
